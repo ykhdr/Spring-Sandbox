5afb4d2e9bbb1a236d54fee69a0d416eb2b55127
ykhdr <yanhudorozhkov@gmail.com>
2023-09-16 02:37:27 +0700

    [LIBRARY] Added edit and show pages for Person

--- library-project/src/main/java/ru/ykhdr/library/controllers/PeopleController.java
+++ library-project/src/main/java/ru/ykhdr/library/controllers/PeopleController.java
@@ -6,10 +6,7 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.ykhdr.library.dao.PersonDao;
 import ru.ykhdr.library.models.Person;
 
@@ -20,25 +17,53 @@ public class PeopleController {
     private final PersonDao personDao;
 
     @GetMapping()
-    public String index(@NotNull Model model){
+    public String index(@NotNull Model model) {
         model.addAttribute("people", personDao.index());
 
         return "people/index";
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute("person") Person person){
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult){
-
-        if(bindingResult.hasErrors()){
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "people/new";
         }
 
         personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id).orElse(null));
+
+        return "people/show";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id).orElse(null));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person, @PathVariable("id") int id, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
+        personDao.update(id, person);
         return "redirect:/people";
     }
 
--- library-project/src/main/java/ru/ykhdr/library/dao/PersonDao.java
+++ library-project/src/main/java/ru/ykhdr/library/dao/PersonDao.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import ru.ykhdr.library.models.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @AllArgsConstructor
@@ -23,6 +24,22 @@ public class PersonDao {
         jdbcTemplate.update("INSERT INTO Person(name, surname, father_name, email, birthday_year) VALUES (?,?,?,?,?)",
                 person.getName(), person.getSurname(), person.getFatherName(), person.getEmail(), person.getBirthdayYear());
     }
+
+    public Optional<Person> show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+    }
+
+    public void update(int id, Person person){
+        jdbcTemplate.update("UPDATE Person SET name=?,surname=?,father_name=?,email=?,birthday_year=? WHERE id = ?",
+                person.getName(),person.getSurname(),person.getFatherName(),person.getEmail(),person.getBirthdayYear(), id);
+    }
+
+
 }
 
 
