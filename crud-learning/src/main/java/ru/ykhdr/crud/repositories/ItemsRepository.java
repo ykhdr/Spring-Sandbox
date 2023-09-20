c592f091e3c28be15279844c34c1e3eb89bfe09c
ykhdr <yanhudorozhkov@gmail.com>
2023-09-21 01:24:17 +0700

    [CRUD] Added pageable request in ItemsRepository

--- crud-learning/src/main/java/ru/ykhdr/crud/repositories/ItemsRepository.java
+++ crud-learning/src/main/java/ru/ykhdr/crud/repositories/ItemsRepository.java
@@ -1,5 +1,8 @@
 package ru.ykhdr.crud.repositories;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.ykhdr.crud.models.Item;
@@ -12,6 +15,7 @@ public interface ItemsRepository extends JpaRepository<Item,Integer> {
 
     List<Item> findByOwner(Person owner);
 
-
     List<Item> findByName(String name);
+
+    @NotNull Page<Item> findAll(@NotNull Pageable pageable);
 }
