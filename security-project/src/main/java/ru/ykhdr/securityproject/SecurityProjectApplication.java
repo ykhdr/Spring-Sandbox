7a2ccd83c61b22bfe306ef2788436ed7cfc50457
ykhdr <yanhudorozhkov@gmail.com>
2023-09-22 12:54:12 +0700

    [SECURITY] Initial spring security project

---
+++ security-project/src/main/java/ru/ykhdr/securityproject/SecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.ykhdr.securityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityProjectApplication.class, args);
+    }
+
+}
---
+++ security-project/src/test/java/ru/ykhdr/securityproject/SecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ykhdr.securityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
