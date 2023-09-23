f1fc19dcd80619d2428dfe84f20944a634884b51
ykhdr <yanhudorozhkov@gmail.com>
2023-09-23 11:43:26 +0700

    [SECURITY] Added filter chain configuration

--- security-project/src/main/java/ru/ykhdr/securityproject/config/SecurityConfig.java
+++ security-project/src/main/java/ru/ykhdr/securityproject/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,8 +24,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
-                .formLogin(config -> config
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/auth/login","/error").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
                         .loginPage("/auth/login")
                         .loginProcessingUrl("/process_login")
                         .defaultSuccessUrl("/hello",true)
